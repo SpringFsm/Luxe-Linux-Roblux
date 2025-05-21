de1e5200c07bb8cd4ec3ccaaf664fde4dd69efe0
SpringFsm <94674858+SpringFsm@users.noreply.github.com>
2025-05-21 15:11:45 +0200

    More backend
    more views added to the api

--- projet_bdd/backend/routes/api.js
+++ projet_bdd/backend/routes/api.js
@@ -33,9 +33,37 @@ router.get('/api/jeux/:id', async (req,res) => {
   }
 });
 
-//Get evaluations utilisateurs
-router.get('/api/eval', async (req,res) =>{
+// Get jeux details
+router.get('/api/jeuxDetails', async (req,res) => {
 
+  let querry = 'SELECT * FROM JEUX';
+  try{
+    const [rows] = await pool.execute(querry);
+    res.json({rows : rows});
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+// Get jeux details by id
+router.get('/api/jeuxDetails/:id', async (req,res) => {
+  const id = req.params.id;
+  let querry = 'SELECT * FROM JEUX WHERE id_jeu = ?';
+  
+  try{
+    const [rows] = await pool.execute(querry, [id]);
+    res.json({rows : rows});
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+});
+
+// Get evaluations utilisateurs
+router.get('/api/eval', async (req,res) =>{
   let querry = 'SELECT * FROM vue_evaluations_utilisateurs';
 
   try{
@@ -48,8 +76,50 @@ router.get('/api/eval', async (req,res) =>{
   }
 })
 
+// Get souhaits utilisateurs
+router.get('/api/souhaits', async (req, res) =>{
+  let querry = 'SELECT * FROM vue_souhaits_utilisateurs';
+
+  try{
+    const [rows] = await pool.execute(querry);
+    res.json({rows :rows});
+    
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+})
+
+// Get jeux mieux notés vue_jeux_mieux_notes
+router.get('/api/mieuxNotes', async (req, res) =>{
+  let querry = 'SELECT * FROM vue_jeux_mieux_notes';
+
+  try {
+    const [rows] = await pool.execute(querry);
+    res.json({rows :rows});
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+})
+
+// Get jeux categories et mecaniques
+router.get('/api/categories', async (req, res) =>{
+  let querry = 'SELECT * FROM vue_jeux_categories_mecaniques';
+
+  try {
+    const [rows] = await pool.execute(querry);
+    res.json({rows :rows});
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Erreur serveur' });
+  }
+})
+
 // Vérifie si un jeu est disponible
-router.get('/disponibilite/:id', async (req, res) => {
+router.get('/api/disponibilite/:id', async (req, res) => {
   const { id } = req.params;
 
   try {
